f21bd9b53ccfc8700151cf2f6e98fb216e1d3230
Ivor Houlker <ivor.houlker@gmail.com>
2022-11-19 19:29:33 +0800

    lint, readme, python

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Node or Python implementations of socket server on local network
 
-Just a test
+I was going to do it in python, but then decided to make a test in node first so I know the code is not the problem.
+
+To connect the pi to an iphone via usb hotspot local network, you need `usbmuxd` - I found out [here](https://support.speedify.com/article/565-tethered-iphone-linux). There is a similar package for ubuntu.
+
+The default local ip of the pi seems to be `172.20.10.2` and the iphone is `172.20.10.1` and it acts as the DNS server. Tested this is as consistent when unplugging/replugging.
+
+T
--- server.py
+++ server.py
@@ -1,40 +1,20 @@
-# run this in the terminal to start server
-# >>>>>> gunicorn --threads 50 server:app
-# ----------------------------------
-# the code about uses gunicorn to start the server
-# it uses 50 threads
-# nameOfFile : the app variable
 import socketio
-
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 
-
-players = []
-
-
 @sio.event
 def connect(sid, environ):
-    print("A New Player Connected: ", sid)
-    if len(players) == 0:
-        sio.emit("position", "Player1", room=sid)
-    if len(players) == 1:
-        sio.emit("position", "Player2", room=sid)
-
-    players.append(sid)
-    sio.enter_room(sid, "game room")
-    print("A new player entered game room")
-
+    print("Got connection: ", sid)
+    return sid
+    
 
 @sio.event
-def move(sid, data):
-    print("MOVE from client: ", data)
+def handshake(sid, data):
+    print("handshake from client: ", data)
     sio.emit("receive", data, room="game room", skip_sid=sid)
-
+    return "hi from the python server"
 
 @sio.event
 def disconnect(sid):
     print("Disconnected SID >> ", sid)
-    players.remove(sid)
-    print("This is now the list: ", players)
